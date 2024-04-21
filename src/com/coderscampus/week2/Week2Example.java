d3fba84379041debf2d7ebcafa6b437c088db7dd
pastormikehope <155399156+pastormikehope@users.noreply.github.com>
2024-04-20 23:43:17 -0400

    Initial commit

---
+++ src/com/coderscampus/week2/Week2Example.java
@@ -0,0 +1,28 @@
+package com.coderscampus.week2;
+
+import java.util.Scanner;
+
+public class Week2Example {
+	public static void main (String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Type in number between 50 and 300");
+		String userInput = scanner.nextLine();
+		Integer convertedInput = Integer.parseInt(userInput);
+		
+		if (userInput.equals ("> 50") && ("< 300")){
+		System.out.println("Yes");
+		} else {
+			System.out.println("No");
+		}
+	
+	
+	}
+	
+	
+		
+		
+	}
+	
+
+
